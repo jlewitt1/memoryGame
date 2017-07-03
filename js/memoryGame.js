8f7f55a9c9656db8e78dbc3bdcb57d1e667d223c
Josh <josh.lewittes@gmail.com>
2017-07-03 18:57:02 +0300

    Update #2
    create board using JS only, removing HTML code

--- js/memoryGame.js
+++ js/memoryGame.js
@@ -1,94 +1,67 @@
 
-var numClicks = 0;
-var firstChoice;
-var secondChoice;
+var body = document.getElementsByTagName("body")[0];
+var memory_board = document.getElementById("memory_board");
 
-var numGames =0;
-var tempCard = './images/temp.jpg';
+var randNum = Math.floor(Math.random() * 6)+ 1; 
 
-var cards = [ 
-	{
-		name: "shakshuka",
-		img: './images/shakshuka.jpg',
-		id: 1,
-	},
+var numCols = 6; 
+var numRows = 4;
 
-	{
-		name: "falafel",
-		img: './images/falafel.jpg',
-		id: 2,
-	},
+var cards = ['shakshuka', 'shwarma', 'falafel', 'schnitzel', 'goldstar', 'sabich', 'maccabi','hummus','eggplant','beach','deadSea','hermon','shakshuka', 'shwarma', 'falafel', 'schnitzel', 'goldstar', 'sabich', 'maccabi','hummus','eggplant','beach','deadSea','hermon'];
 
-	{
-		name: "sabich",
-		img: './images/sabich.jpg',
-		id:3,
-	},
+function createCards(){ //create the memory board
+	for (var i=0; i<numRows; i++) { 
+		var row = document.createElement("div");
+		row.className = "row";
+		memory_board.appendChild(row);
 
-	{
-		name: "shwarma",
-		img: './images/shwarma.jpg',
-		id:4,
-	},
-
-	{ 
-		name: "schnitzel",
-		img: './images/schnitzel.jpg',
-		id: 5,
-	},
-
-	{ 
-		name: "goldstar",
-		img: './images/goldstar.jpg',
-		id: 6,
-	},
-];	
-
-	
-	function choose (cardPicked) {  
-		if (numClicks == 2){ 
-		return;
-		}
-		else if (numClicks == 0) { 
-		  firstChoice = cardPicked;
-		  document.images[cardPicked].src = cards[cardPicked];
-		  numClicks =1;
-		}
-		else { 
-		  numClicks= 2;
-		  secondChoice = cardPicked;
-		  document.images[cardPicked].src = cards[cardPicked];
-		  timer = setInterval("check()", 1000);
+		for (var j=0; j<numCols; j++){ 
+		 var card = document.createElement("div");
+		 card.className="card col-md-3";
+		 row.appendChild(card);
+		 card.addEventListener('click',changeBackground);
 		}
 	}
-
-	function check() { // Checking if there is a match 
-
-    clearInterval(timer); //stop timer
-
-    numClicks = 0;
-    if (cards[secondchoice] == cards[firstchoice]) {
-        numGames++;
-        document.getElementById("numGames").innerHTML = "Number of Games";
-    } else {
-        document.images[firstchoice].src = tempCard;
-        document.images[secondchoice].src = tempCard;
-        return;
-    }
 }
 
-   	
+var DOMcards = document.getElementsByClassName("card");
+function setAttributes() {
+	for (var i=0; i<cards.length;i++) { 
+		DOMcards[i].setAttribute('data-name',cards[i]);
+	}
+}
 
-	.addEventListener("click", function() { //function to change image that was clicked on from temp to current
-	
-		document.getElementById("") //get picture that was clicked on
+function changeBackground(event) {  //change image from temp image
+	var name = 	event.target.getAttribute("data-name");
+	event.target.style.backgroundImage = 'url(images/' + name + '.jpg)';
+	event.target.backgroundSize = "cover";
+	changeImageBack();
+}
+
+function changeImageBack () { //after clicking switch back to temp image
+	card.style.backgroundImage = 'url(images/temp.jpg)';
+
+
+}
+
+function gameOver () { //if user wins
 
 
 
+alert ("Congrats - You win!");
+startNewGame(); // call function to display newGame button
+}
 
-	});
+function startNewGame () { 
+	var newGame = document.createElement("button");
+	memory_board.appendChild(newGame);
+	newGame.innerHTML = "New Game!";
+	Object.assign(newGame.style,{backgroundColor:"darkblue", borderColor:"lightgray", borderRadius:"6px", height:"60px", width:"80px",});
+}
 
-
+createCards();
+setAttributes();
+setTimeout(changeImageBack,500); //set lag time after clicking on two pictures that don't much
 
 
 
