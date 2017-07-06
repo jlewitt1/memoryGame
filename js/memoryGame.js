2bce5a7bf8cde450fc88106e91050257ff378a57
Josh <josh.lewittes@gmail.com>
2017-07-06 10:26:33 +0300

    update #6
    css updates, guess count

--- js/memoryGame.js
+++ js/memoryGame.js
@@ -5,6 +5,9 @@ var memory_board = document.getElementById("memory_board");
 var numCols = 6; 
 var numRows = 3;
 var totalMatches;
+var numClicks=0;
+var newGame = document.createElement("button");
+var results = document.createElement("span");
 
 var cards = ['shakshuka', 'shwarma', 'falafel', 'schnitzel', 'goldstar', 'sabich', 'maccabi','hummus','eggplant',
 			 'shakshuka', 'shwarma', 'falafel', 'schnitzel', 'goldstar', 'sabich', 'maccabi','hummus','eggplant'];
@@ -14,8 +17,12 @@ var DOMcards = document.getElementsByClassName("card");
 gameStart();
 
 function gameStart () { 
+	body.style.backgroundImage= "";
 	memory_board.innerHTML = ''; //re-set the board to allow for multiple games
 	totalMatches = ((numCols * numRows) / 2);
+	numClicks=0;
+	newGame.innerHTML = "";
+	results.innerHTML = "";
 	shuffleCards(cards);
 	createCards();
 	setAttributes();
@@ -52,7 +59,7 @@ function createCards() { //create the memory board
 function setAttributes() { //setting name of each element in cards array
 	for (var i=0; i<cards.length;i++) { 
 		DOMcards[i].setAttribute('data-name',cards[i]);
-		DOMcards[i].innerHTML = DOMcards[i].getAttribute('data-name');
+		DOMcards[i].innerHTML = DOMcards[i].getAttribute('data-name'); //-->see answers if don't want to guess
 	}
 }
 
@@ -63,6 +70,7 @@ function changeBackground(event) {  //change image from temp image
 		var name = 	event.target.getAttribute("data-name");
 		var id = event.target;
 		event.target.style.backgroundImage = 'url(images/' + name + '.jpg)';
+		numClicks++;
 		event.target.style.pointerEvents = 'none';
 		tempArray.push(name);
 		tempArray.push(id);
@@ -75,14 +83,12 @@ function finalCheck () { //determine whether there is a match
 			if (tempArray[0] == tempArray[2]) {  //if there is a match
 				tempArray=[];
 				totalMatches--;
-				//console.log(totalMatches);
 				checkGameOver();
 			} else {  //if there is no match 
 				setTimeout(function() { //set lag time after clicking on two pictures that don't match
 					changeImageBack();
 					tempArray=[];
-
-				}, 1200)	
+				}, 1000)	
 			}		
 	} 
 }
@@ -94,16 +100,23 @@ function changeImageBack () { //after clicking switch back to temp image
 
 function checkGameOver () { //if game is over prompt user to start a new one
 	if (totalMatches == 0) { 
-		setTimeout(function(){alert("Congratulations you won!")},100);
+		setTimeout(100);
 		newGamePrompt();
 	}
 }
 
 function newGamePrompt () { //after previous game is finished show button to allow new game on click
-	var newGame = document.createElement("button");
+	body.style.backgroundImage = 'url("https://media.giphy.com/media/26tOZ42Mg6pbTUPHW/giphy.gif")';
 	newGame.innerHTML = "New Game!";
 	body.appendChild(newGame);
-	newGame.addEventListener('click',gameStart);
+	results.innerHTML = "Number of guesses: " + (numClicks/2);
+	body.appendChild(results);
+	newGame.addEventListener('click',hideButton);
+}
+
+function hideButton () { //hide the button after it is clicked before new game begins
+	newGame.style.display = 'none';
+	gameStart();
 }
 
 
